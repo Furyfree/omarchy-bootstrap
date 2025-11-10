270e927b898ef875d9eab9f0e4a8638e6dc385e3
Patrick Byrne <pby.software@byrne.dk>
2025-11-10 18:02:50 +0100

    Fix path to Hyprland overrides config

--- scripts/40-hypr.sh
+++ scripts/40-hypr.sh
@@ -4,7 +4,7 @@ set -e
 echo "==> [40] Setting up Hyprland overrides"
 
 HYPRLAND_CONFIG="$HOME/.config/hypr/hyprland.conf"
-OVERRIDES_CONFIG="$HOME/git/dotfiles-2.0/hyprland/.config/overrides.conf"
+OVERRIDES_CONFIG="$HOME/git/dotfiles-2.0/hyprland/.config/hypr/overrides.conf"
 SOURCE_LINE="source = $OVERRIDES_CONFIG"
 
 # Check if Hyprland config exists
